63a5c2dcbd75cb48810a2ca10209d026c1979a39
Flavio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-20 11:01:21 -0300

    Adiciona captura de digitação com contagem

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,16 @@
-var frase = $(".frase");
+var frase = $(".frase").text();
 var numeroPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase").text();
-
+var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numeroPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function () {
+    var conteudo = campo.val();
+
+    var qtdePalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdePalavras);
+
+    var qtdeCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdeCaracteres);
+});
+
